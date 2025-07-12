6f6bda956d03a30872a27662af2c24986b5ac7a2
Gaardev <benjaminreinertsen@gmail.com>
2025-07-12 16:23:07 +0200

    Set up classes

--- models/v0.1/main.py
+++ models/v0.1/main.py
@@ -16,7 +16,7 @@ import whisper
 
 # PERMISSION INTEGER: 328568269824 (USED FOR GETTING SAME PERMISSIONS EACH TIME YOU INVITE)
 
-model = whisper.load_model("medium")
+model = whisper.load_model("base")
 audio_queue = queue.Queue()
 
 # -- GETTING ENV -- #
@@ -33,8 +33,56 @@ GEMNI_API_KEY = os.getenv("GEMNI_API")
 
 # -- SETTING UP CLASSES -- #
 
+class VoiceSession:
+    def __init__(self, user, channel):
+        pass
+
+    async def connect(self):
+        pass
+
+    async def disconnect(self):
+        pass
+
+    async def start_recording(self):
+        pass
+
+    async def stop_recording(self):
+        pass
+
+class AudioRecorder:
+    def __init__(self, voice_client):
+        pass
+
+    async def record(self):
+        pass
+
+    def get_audio_chunk(self):
+        pass
 
 
+class TranscriptionManager:
+    def __init__(self):
+        self.queue = ChunkQueue()
+        self.transcribed_results = []
+        
+    async def transcribe_chunk(self, audio_chunk):
+        pass
+
+    async def worker(self):
+        pass
+
+class ChunkQueue:
+    def __init__(self):
+        pass
+
+    def add(self, chunk):
+        pass
+
+    def get(self):
+        pass
+
+    def is_empty(self):
+        pass
 # -- DEFINING GENAI -- #
 
 def gemni(payload, API_KEY):
@@ -58,18 +106,36 @@ tree = bot.tree
 # -- SLASH COMMANDS -- #
 
 @bot.tree.command(
-    name="sayhello",
-    description="Say hello in a specified voice channel"
+    name="join",
+    description="Join a voice channel"
 )
 @app_commands.describe(channel="Choose a voice channel")
 async def sayhello(interaction: discord.Interaction, channel: discord.VoiceChannel):
-    await interaction.response.send_message(f"Hello from {channel.name}!", ephemeral=True)
+    if interaction.guild.voice_client:
+        await interaction.guild.voice_client.disconnect()
+    await channel.connect()
+    await interaction.response.send_message(f"Joined: {channel.name}", ephemeral=True)
+
+@bot.tree.command(
+    name="leave",
+    description="Leave a voice channel"
+)
+@app_commands.describe()
+async def sayhello(interaction: discord.Interaction):
+    voice_client = interaction.guild.voice_client
+
+    if voice_client and voice_client.is_connected():
+        channel_name = voice_client.channel.name
+        await voice_client.disconnect()
+        await interaction.response.send_message(f"Disconnected from {channel_name}", ephemeral=True)
+    else:
+        await interaction.response.send_message("Not in a voice channel", ephemeral=True)
 
 @bot.event
 async def on_ready():
     guild = discord.Object(id=1387414175952932984)
     bot.tree.clear_commands(guild=guild)
-    await bot.tree.sync(guild=guild)
+    await bot.tree.sync()
     print("Commands synced to test guild")
     print(f"Logged in as {bot.user} (ID: {bot.user.id}) - slash commands synced")
 
